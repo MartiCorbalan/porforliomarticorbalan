cf677d3d045bd59358c17d5f3e9f89afb78ccd74
administrador <martilluc01@gmail.com>
2022-01-20 21:02:09 +0100

    arreglar menu

---
+++ projecte1/js/main.js
@@ -0,0 +1,27 @@
+let prevScrollpos = window.pageYOffset;
+
+window.onscroll = function () {
+  let currentScrollPos = window.pageYOffset;
+  if (prevScrollpos > currentScrollPos) {
+    document.getElementById("navbar").style.top = "0";
+  } else {
+    document.getElementById("navbar").style.top = "-100px";
+  }
+  prevScrollpos = currentScrollPos;
+};
+
+
+let enlacesHeader = document.querySelectorAll(".enlaces-header")[0];
+let semaforo = true;
+/*si el semaforo que en este caso es el icono del menu es true, estara en blanco, es decir el menu esta abierto, y si esta cerrado es decir false, sea negro*/
+document.querySelectorAll(".hamburguer")[0].addEventListener("click", function() {
+    if (semaforo) {
+        document.querySelectorAll(".hamburguer")[0].style.color = "#fff";
+        semaforo = false;
+    } else {
+        document.querySelectorAll(".hamburguer")[0].style.color = "#000";
+        semaforo = true;
+    }
+    enlacesHeader.classList.toggle("menudos")
+})
+
---
+++ projecte1/js/menu.js
@@ -0,0 +1,38 @@
+(function(){
+    const listElements = document.querySelectorAll('.menu_item--show');
+    const list = document.querySelector('.menu_links');
+    const menu = document.querySelector('.menu_hamburguer');
+  
+    const addClick = ()=>{
+      listElements.forEach(element => {
+        element.addEventListener('click', ()=>{
+          let subMenu = element.children[1];
+          element.classList.toggle('menu_item--active');
+  
+  
+        });
+      });
+    }
+    const deleteStyleHeigth = ()=>{
+      listElements.forEach(element=>{
+        if(element.children[1].getAttribute('style')){
+          element.children[1].removeAttribute('style');
+          element.classList.remove('menu_item--active');
+        }
+      })
+    }
+  
+    window.addEventListener('resize', ()=>{
+      if(window.innerHeight > 800){
+        deleteStyleHeigth();
+      }else{
+        addClick();
+      }
+    });
+    if(window.innerHeight <= 800){
+      addClick();
+    }
+  
+    menu.addEventListener('click', ()=> list.classList.toggle('menu_links--show'));
+  
+  })();
